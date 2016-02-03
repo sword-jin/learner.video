45b07261d9ed971d63176c252d11458c89f4756c
RryLee <qq215672398@126.com>
2016-02-03 22:08:29 +0800

    fix

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="{{ elixir("css/app.css") }}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta property="og:title" content="{{ asset('layouts.meta_title') }}"/>
+    <meta property="og:title" content="{{ trans('layouts.meta_title') }}"/>
     <meta property="og:image" content="{{ asset('img/logo.png') }}" />
     <meta property="og:description" content="@yield('description')" />
     <meta name="keywords" content="@yield('keywords', trans('layouts.meta_keywords'))">
@@ -33,7 +33,7 @@
 <script src="//cdn.bootcss.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 @yield('script')
 @include('flashy::message')
-@yield('partials.analytics.baidu')
-@yield('partials.analytics.google')
+@include('partials.analytics.baidu')
+@include('partials.analytics.google')
 </body>
 </html>
